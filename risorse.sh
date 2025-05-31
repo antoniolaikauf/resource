61ed0204fb1ced1e95685974e167473ca0ce5efe
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-05-31 11:42:58 +0200

    memoria

--- risorse.sh
+++ risorse.sh
@@ -1,8 +1,23 @@
 #  caratteristiche della cpu
-ARCHITETTURA_CPU=`lscpu | grep "Architecture" | awk -F: '{print $2}'`
-MODEL_NAME=`lscpu | grep "Model name" | awk -F: '{print $2}' | xargs`
-NUMBER_CORE=`lscpu | grep "^CPU(s)" | awk -F: '{print $2}' | xargs`
-NUMBER_CORE_ON=`lscpu | grep "On-line CPU(s) list:" | awk -F: '{print $2}'`
+ARCHITETTURA_CPU=` lscpu | grep "Architecture" | awk -F: '{print $2}'`
+MODEL_NAME=` lscpu | grep "Model name" | awk -F: '{print $2}' | xargs`
+NUMBER_CORE=` lscpu | grep "^CPU(s)" | awk -F: '{print $2}' | xargs`
+NUMBER_CORE_ON=` lscpu | grep "On-line CPU(s) list:" | awk -F: '{print $2}'`
+
+# -------------------------------
+#          MEMORIA
+# -------------------------------
+
+MEMORY_TOTAL=` free -b | awk '/Mem:/ {print$2}'`
+MEMORY_USED=` free -b | awk '/Mem:/ {print$3}'`
+MEMORY_FREE=` free -b | awk '/Mem:/ {print$4}'`
+ALL_MEMORY_FREE=` free -b | awk '/Mem:/ {print$6}'`
+PERCENTUALE_MEMOR=$(echo "scale=1; ($MEMORY_USED * 100) / $MEMORY_TOTAL" | bc)
+
+VIRTUAL_MEMORY_TOTAL=` free -b | awk '/Swap:/ {print$2}'`
+VIRTUAL_MEMORY_USED=` free -b | awk '/Swap:/ {print$3}'`
+VIRTUAL_MEMORY_FREE=`free -b | awk '/Swap:/ {print$4}'`
+VIRTUAL_PERCENTUALE_MEMOR=$(echo "scale=1; ($VIRTUAL_MEMORY_USED * 100) / $VIRTUAL_MEMORY_TOTAL" | bc)
 
 # stty size | perl -ale 'print "-"x$F[1]'
 
@@ -10,13 +25,15 @@ NUMBER_CORE_ON=`lscpu | grep "On-line CPU(s) list:" | awk -F: '{print $2}'`
 GPU=`lspci | grep -E "VGA|3D|Display" | grep -v Microsoft` 
 echo $GPU
 
-if [ -z "$GPU" ]; then
+if [ -n "$GPU" ]; then
     echo "non è installata una GPU"
-    echo "The value is: $MODEL_NAME"
-    . ./cpu.sh
-else
-    . ./cpu.sh
-    . ./gpu.sh
+    echo "il modello di CPU è: $MODEL_NAME"
     echo $GPU
-    echo echo $MODEL_NAME
+    . ./gpu.sh
 fi
+
+echo "non è installata una GPU"
+echo "il modello di CPU è: $MODEL_NAME"
+. ./cpu.sh
+
+# help test 
